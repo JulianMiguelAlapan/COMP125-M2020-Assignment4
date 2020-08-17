6757b7cf1812f1dc351a8b5199b63ae4bc294ca3
Julian Miguel Alapan <jalapan1@my.centennialcollege.ca>
2020-08-17 19:17:26 -0400

    added canvas/stage & framerate

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,5 +4,20 @@ Author:Julian Miguel Alapan
 Student#:300836721
 WebsiteName:COMP125-2020-Assignment4
 Description:Slot Machine
-*/ 
+*/
+(function () {
+    // Function Scoped Variables
+    var stage;
+    function Start() {
+        var canvas = document.getElementById("canvas");
+        var stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 FPS or 16.667 ms
+        createjs.Ticker.on("tick", Update);
+        console.log("App Started...");
+    }
+    function Update() {
+        stage.update();
+    }
+    window.addEventListener("load", Start);
+})();
 //# sourceMappingURL=app.js.map
--- Scripts/app.ts
+++ Scripts/app.ts
@@ -5,3 +5,24 @@ Student#:300836721
 WebsiteName:COMP125-2020-Assignment4
 Description:Slot Machine
 */
+(function(){
+    // Function Scoped Variables
+    let stage: createjs.Stage;
+
+    function Start():void
+    {
+        let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        let stage = new createjs.Stage(canvas)
+        createjs.Ticker.framerate = 60; // 60 FPS or 16.667 ms
+        createjs.Ticker.on("tick", Update);
+
+        console.log("App Started...");
+    }
+
+    function Update():void
+    {
+        stage.update();
+    }
+
+    window.addEventListener("load", Start);
+})();
