2f50537d109bc6ef112e44574124599920b50234
Julian Miguel Alapan <jalapan1@my.centennialcollege.ca>
2020-08-17 18:52:09 -0400

    added internal documentation

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,8 @@
+/*
+Name:COMP125-M2020-Assignment4
+Author:Julian Miguel Alapan
+Student#:300836721
+WebsiteName:COMP125-2020-Assignment4
+Description:Slot Machine
+*/ 
 //# sourceMappingURL=app.js.map
---
+++ Scripts/app.ts
@@ -0,0 +1,7 @@
+/*
+Name:COMP125-M2020-Assignment4
+Author:Julian Miguel Alapan
+Student#:300836721
+WebsiteName:COMP125-2020-Assignment4
+Description:Slot Machine
+*/
