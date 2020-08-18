8f6ecd28f46bfad011fd029b3085c6c3b181d33e
Julian Miguel Alapan <jalapan1@my.centennialcollege.ca>
2020-08-17 21:14:47 -0400

    added more to button.ts

--- Scripts/uiobjects/button.js
+++ Scripts/uiobjects/button.js
@@ -1,11 +1,30 @@
 var UIObjects;
 (function (UIObjects) {
     class Button extends createjs.Bitmap {
-        // PRIVATE FIELDS (CLASS MEMBERS)
-        // PUBLIC PROPERTIES
         // CONSTRUCTOR(S)
-        constructor() {
-            super();
+        constructor(bitmap_asset, x = 0, y = 0, isCentered = false) {
+            super(Config.Globals.AssestManifest.getResult(bitmap_asset));
+            this.isCentered = isCentered;
+            this.x = x;
+            this.y = y;
+        }
+        // PUBLIC PROPERTIES
+        get isCentered() {
+            return this.m_isCentered;
+        }
+        set isCentered(value) {
+            if (value) {
+                this.m_recalculateSize();
+            }
+            else {
+                this.regX = 0;
+                this.regY = 0;
+            }
+        }
+        // PRIVATE METHOD(S)
+        m_recalculateSize() {
+            this.regX = this.getBounds().width * 0.5;
+            this.regY = this.getBounds().height * 0.5;
         }
     }
     UIObjects.Button = Button;
--- Scripts/uiobjects/button.ts
+++ Scripts/uiobjects/button.ts
@@ -3,15 +3,42 @@ module UIObjects
     export class Button extends createjs.Bitmap
     {
         // PRIVATE FIELDS (CLASS MEMBERS)
+        private m_isCentered:boolean;
 
         // PUBLIC PROPERTIES
-
-        // CONSTRUCTOR(S)
-        constructor()
+        get isCentered():boolean
         {
-            super()
+            return this.m_isCentered;
+        }
+
+        set isCentered(value:boolean)
+        {
+            if(value)
+            {
+                this.m_recalculateSize();
+            }
+            else
+            {
+                this.regX = 0;
+                this.regY = 0;
+            }
+        }
+        // CONSTRUCTOR(S)
+        constructor(bitmap_asset:string, x:number = 0, y:number = 0, isCentered:boolean = false)
+        {
+            super(Config.Globals.AssestManifest.getResult(bitmap_asset))
+
+            this.isCentered = isCentered;
+
+            this.x = x;
+            this.y = y;
         }
         // PRIVATE METHOD(S)
+        private m_recalculateSize():void
+        {
+            this.regX = this.getBounds().width * 0.5;
+            this.regY = this.getBounds().height * 0.5;
+        }
 
         // PUBLIC METHOD(S)
     }
