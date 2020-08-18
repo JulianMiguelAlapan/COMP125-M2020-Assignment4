29368b56ad5dd666e0f4fba31aafe6b7f0481c55
Julian Miguel Alapan <jalapan1@my.centennialcollege.ca>
2020-08-17 22:38:21 -0400

    added pictures and labels

--- Scripts/core/app.js
+++ Scripts/core/app.js
@@ -17,8 +17,12 @@ Description:Slot Machine
     let betMaxButton;
     let jackPotLabel;
     let creditLabel;
-    let winningLabel;
+    let winningsLabel;
     let betLabel;
+    let leftReel;
+    let middleReel;
+    let rightReel;
+    let betLine;
     let manifest = [
         { id: "background", src: "./Assets/images/background.png" },
         { id: "banana", src: "./Assets/images/banana.gif" },
@@ -48,7 +52,7 @@ Description:Slot Machine
     function Start() {
         console.log("App Started...");
         let canvas = document.getElementById("canvas");
-        let stage = new createjs.Stage(canvas);
+        stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 FPS or 16.667 ms
         createjs.Ticker.on("tick", Update);
         stage.enableMouseOver(20);
@@ -61,8 +65,38 @@ Description:Slot Machine
     }
     function buildInterface() {
         // Slot Machine Background
-        slotMachineBackground = new Core.GameObject("background", 120, 240, true);
+        slotMachineBackground = new Core.GameObject("background", Config.Screen.CENTER_X, Config.Screen.CENTER_Y, true);
         stage.addChild(slotMachineBackground);
+        // Buttons
+        spinButton = new UIObjects.Button("spinButton", Config.Screen.CENTER_X + 135, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(spinButton);
+        bet1Button = new UIObjects.Button("bet1Button", Config.Screen.CENTER_X - 135, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(bet1Button);
+        bet10Button = new UIObjects.Button("bet10Button", Config.Screen.CENTER_X - 67, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(bet10Button);
+        bet100Button = new UIObjects.Button("bet100Button", Config.Screen.CENTER_X, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(bet100Button);
+        betMaxButton = new UIObjects.Button("betMaxButton", Config.Screen.CENTER_X + 67, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(betMaxButton);
+        // Labels
+        jackPotLabel = new UIObjects.Label("99999999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X, Config.Screen.CENTER_Y - 175, true);
+        stage.addChild(jackPotLabel);
+        creditLabel = new UIObjects.Label("99999999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X - 94, Config.Screen.CENTER_Y + 108, true);
+        stage.addChild(creditLabel);
+        winningsLabel = new UIObjects.Label("99999999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X + 94, Config.Screen.CENTER_Y + 108, true);
+        stage.addChild(winningsLabel);
+        betLabel = new UIObjects.Label("9999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X, Config.Screen.CENTER_Y + 108, true);
+        stage.addChild(betLabel);
+        // Reel GameObjects
+        leftReel = new Core.GameObject("bell", Config.Screen.CENTER_X - 79, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(leftReel);
+        middleReel = new Core.GameObject("banana", Config.Screen.CENTER_X, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(middleReel);
+        rightReel = new Core.GameObject("bar", Config.Screen.CENTER_X + 78, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(rightReel);
+        // Bet Line
+        betLine = new Core.GameObject("bet_line", Config.Screen.CENTER_X, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(betLine);
     }
     // app logic goes here
     function Main() {
--- Scripts/core/app.ts
+++ Scripts/core/app.ts
@@ -17,8 +17,12 @@ Description:Slot Machine
     let betMaxButton: UIObjects.Button;
     let jackPotLabel: UIObjects.Label;
     let creditLabel: UIObjects.Label;
-    let winningLabel: UIObjects.Label;
+    let winningsLabel: UIObjects.Label;
     let betLabel: UIObjects.Label;
+    let leftReel: Core.GameObject;
+    let middleReel: Core.GameObject;
+    let rightReel: Core.GameObject;
+    let betLine: Core.GameObject;
     
     let manifest: Core.Item[] = [
         {id:"background", src:"./Assets/images/background.png"},
@@ -54,7 +58,7 @@ Description:Slot Machine
     {
         console.log("App Started...");
         let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-        let stage = new createjs.Stage(canvas)
+        stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 FPS or 16.667 ms
         createjs.Ticker.on("tick", Update);
 
@@ -74,8 +78,51 @@ Description:Slot Machine
     function buildInterface():void
     {
         // Slot Machine Background
-        slotMachineBackground = new Core.GameObject("background", 120, 240, true );
+        slotMachineBackground = new Core.GameObject("background", Config.Screen.CENTER_X, Config.Screen.CENTER_Y, true );
         stage.addChild(slotMachineBackground);
+
+        // Buttons
+        spinButton = new UIObjects.Button("spinButton", Config.Screen.CENTER_X + 135, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(spinButton);
+
+        bet1Button = new UIObjects.Button("bet1Button", Config.Screen.CENTER_X - 135, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(bet1Button);
+
+        bet10Button = new UIObjects.Button("bet10Button", Config.Screen.CENTER_X - 67, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(bet10Button);
+
+        bet100Button = new UIObjects.Button("bet100Button", Config.Screen.CENTER_X, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(bet100Button);
+
+        betMaxButton = new UIObjects.Button("betMaxButton", Config.Screen.CENTER_X + 67, Config.Screen.CENTER_Y + 176, true);
+        stage.addChild(betMaxButton);
+
+        // Labels
+        jackPotLabel = new UIObjects.Label("99999999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X, Config.Screen.CENTER_Y - 175, true);
+        stage.addChild(jackPotLabel);
+
+        creditLabel = new UIObjects.Label("99999999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X - 94, Config.Screen.CENTER_Y + 108, true);
+        stage.addChild(creditLabel);
+
+        winningsLabel = new UIObjects.Label("99999999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X + 94, Config.Screen.CENTER_Y + 108, true);
+        stage.addChild(winningsLabel);
+
+        betLabel = new UIObjects.Label("9999", "20px", "Consolas", "#FF0000", Config.Screen.CENTER_X, Config.Screen.CENTER_Y + 108, true);
+        stage.addChild(betLabel);
+
+        // Reel GameObjects
+        leftReel = new Core.GameObject("bell", Config.Screen.CENTER_X - 79, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(leftReel);
+
+        middleReel = new Core.GameObject("banana", Config.Screen.CENTER_X, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(middleReel);
+
+        rightReel = new Core.GameObject("bar", Config.Screen.CENTER_X + 78, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(rightReel);
+
+        // Bet Line
+        betLine = new Core.GameObject("bet_line", Config.Screen.CENTER_X, Config.Screen.CENTER_Y - 12, true);
+        stage.addChild(betLine);
     }
 
     // app logic goes here
